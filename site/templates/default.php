6eb82f4b5520a000cfdc686ccb8e9d7946480957
Hristiyan Dodov <h.dodov@gmail.com>
2020-06-16 19:32:20 +0300

    doc: intro changes

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,11 +2,11 @@
 
 <aside class="b-sidebar fixed top-0 left-0 px-4 h-screen bg-gray-200">
 	<a href="<?= $site->url() ?>">
-		<img class="h-8 mx-auto my-8" src="<?= $site->file('logo.svg')->url() ?>" alt="">
+		<img class="h-8 mx-auto my-8" src="<?= $site->file('logo.svg')->url() ?>" alt="logo">
 	</a>
 
 	<ul class="my-4">
-		<?php foreach ($pages as $entry) : ?>
+		<?php foreach ($pages->listed() as $entry) : ?>
 			<li>
 				<a class="b-btn flex py-1 <?= e($page === $entry, 'is-active') ?>" href="<?= $entry->url() ?>">
 					<?= $entry->title() ?>
