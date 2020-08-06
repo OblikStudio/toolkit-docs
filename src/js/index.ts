bb9a0661d931046132242e0fa0113f92d15876c0
Hristiyan Dodov <h.dodov@gmail.com>
2020-08-06 08:35:23 +0300

    doc: browser util

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,8 +5,6 @@ import './modules/keyboard.ts'
 import { Watcher } from './oblik/src/index'
 import scrollto from './oblik/src/components/scroll-to'
 import tabs from './modules/tabs'
-import { Animation } from 'oblik/utils'
-import { easeOutQuad } from 'oblik/utils/easings'
 
 let w = new Watcher(document.body, {
 	components: {
@@ -17,7 +15,3 @@ let w = new Watcher(document.body, {
 w.init()
 
 document.addEventListener('click', scrollto.clickHandler())
-
-let anim = new Animation(500, easeOutQuad)
-anim.on('update', console.log)
-anim.run()
