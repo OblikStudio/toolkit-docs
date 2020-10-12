c93f39ae7213dab9479cb1bf12289108ac6d8808
Hristiyan Dodov <h.dodov@gmail.com>
2020-10-12 11:19:01 +0300

    feat: add meta plugin

--- site/snippets/document/open.php
+++ site/snippets/document/open.php
@@ -4,9 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>
-		<?= $page->title() ?> &bull; <?= $site->title() ?>
-	</title>
+	<?= snippet('meta') ?>
 
 	<?= css('assets/main.css') ?>
 </head>
